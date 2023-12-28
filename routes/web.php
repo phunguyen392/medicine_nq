889de6203deb237467b943e1bb487b3b57a61966
nguyen phu <nguyenphu.126@gmail.com>
2023-12-28 16:45:08 +0700

    layout-login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/layouts', function () {
+    return view('admin.master');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/login', App\Http\Controllers\Auth\LoginController::class)->name('admin.login');
